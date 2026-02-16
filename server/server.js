5565ef8b159ea4a14619443e3c9717cd12721234
sou24vik <dassouvik188@gmail.com>
2026-02-16 21:39:44 +0530

    add additional CORS origin for deployment

--- server/server.js
+++ server/server.js
@@ -14,7 +14,7 @@ await connectDB()
 
 app.use(express.json())
 app.use(cors({
-    origin: ["http://localhost:5173", "http://localhost:3000", "https://resume-ri3rajv14-souvik-das-projects-3bda5339.vercel.app"],
+    origin: ["http://localhost:5173", "http://localhost:3000", "https://resume-ri3rajv14-souvik-das-projects-3bda5339.vercel.app", "https://resume-os-server.vercel.app"],
     credentials: true
 }))
 
